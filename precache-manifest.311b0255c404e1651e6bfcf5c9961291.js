6af1d31c9b1406dd79755073ddf0470a00fb3586
soyokaze <07sameersaini@gmail.com>
2020-07-23 11:04:20 +0530

    Updates

---
+++ precache-manifest.311b0255c404e1651e6bfcf5c9961291.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424c2334679e73f208116e8ecca46d0e",
+    "url": "/react-exp/index.html"
+  },
+  {
+    "revision": "8d62d11b446874e7816e",
+    "url": "/react-exp/static/css/main.47392a05.chunk.css"
+  },
+  {
+    "revision": "7103ffd48d52a7ee245b",
+    "url": "/react-exp/static/js/2.6efdbd2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-exp/static/js/2.6efdbd2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d62d11b446874e7816e",
+    "url": "/react-exp/static/js/main.c819905b.chunk.js"
+  },
+  {
+    "revision": "61fe17d8c292d9260153",
+    "url": "/react-exp/static/js/runtime-main.d44a4eb0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-exp/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.69fe992584b0e9ff1f003250d9b400ae.js"
+  "/react-exp/precache-manifest.311b0255c404e1651e6bfcf5c9961291.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-exp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
